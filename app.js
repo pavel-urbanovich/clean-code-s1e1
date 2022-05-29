16f12be05f8bd8b1701570cd888d44e048a068d8
Pavel Urbanovich <yrbik@mail.ru>
2022-05-29 11:25:12 +0300

    refactor: add classlist in app.js and change name input (no rule)

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
@@ -46,6 +46,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
+    listItem.classList.add('todo-block');
 
 
     //and appending.
